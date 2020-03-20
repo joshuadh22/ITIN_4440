020954118cbb5fd6d431dab899fc517c7cdb9e79
annadrewnewby <anewby@unomaha.edu>
2020-03-19 22:14:27 -0500

    these changes DO NOT work, there is something up with the ts file. Otherwise, this parses in info from add org form

--- share-our-table/src/app/organizations/add-organizations-dialog.component.ts
+++ share-our-table/src/app/organizations/add-organizations-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 //import { FormControl } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Organization } from '../organization';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-organizations',
@@ -12,22 +14,28 @@ export class AddOrganizationsDialogComponent implements OnInit {
   message:string;
   action:string = "Dismiss";
   fileUploaded = false;
+  private orgCollection: AngularFirestoreCollection<Organization>;
+  orgName: string;
 
-  constructor(private _snackBar: MatSnackBar) { }
-
-
-  save() {
-
-  //   if (this.fileUploaded === true)
-  //   {
-  //     this.message = 'Organization Creation successfull!';
-  //   }
-  //   else
-  //   {
-  //     this.message = 'Organization Creation Failed! Please try again.'
-  //   }
+  constructor(private _snackBar: MatSnackBar, private afs: AngularFirestore)
+  { 
+    this.orgCollection = afs.collection<Organization>('organizations', ref => ref.where('name', '==', this.orgName));
   }
-  // }
+
+  //still need to bring in the image
+  save(nameIn: string, contactInfoIn: string, descriptionIn: string, linkIn: string) {
+
+    //this.orgCollection.add({name: nameIn, contactInfo: contactInfoIn, description: descriptionIn, image: imageIn, link: linkIn})
+
+    this.afs.collection("organizations").doc(nameIn).set({
+      name: nameIn,
+      contactInfo: contactInfoIn,
+      description: descriptionIn,
+      //image: imageIn,
+      link: linkIn,
+  })
+  }
+
 
   ngOnInit(): void {
   }
